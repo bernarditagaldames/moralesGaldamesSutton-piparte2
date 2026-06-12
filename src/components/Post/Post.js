b22022529b528bd11d7c1e85be4c4ffab24846b5
Iosef-Sutton <isuttondabbah@udesa.edu.ar>
2026-06-12 14:10:30 -0300

    Cree el home

---
+++ src/components/Post/Post.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, Pressable } from "react-native";
+
+export default function Post(props){
+    return(
+        <View>
+            <Text>{props.data.creador}</Text>
+            <Text>{props.data.descripcion}</Text>
+            <Pressable onPress={() => props.navigation.navigate('ComentarPost', { id: props.id })}>
+                <Text>Comentar</Text>
+            </Pressable>
+        </View>
+    )
+}
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,9 +1,50 @@
-import { View, Text } from "react-native";
+import React from "react";
+import { useState, useEffect } from "react";
+import { auth, db } from "../../firebase/config";
+import { View, Text, StyleSheet, ActivityIndicator, Pressable, FlatList } from "react-native";
 
-export default function Home() {
-  return (
-    <View style={{ flex: 1 }}>
-      <Text>Home</Text>
-    </View>
-  );
+import Post from "../../components/Post/Post";
+
+export default function Home(props){
+
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect( () => {
+        auth.onAuthStateChanged( user => {
+            if (!user) {
+                props.navigation.navigate('login');
+            }
+        });
+    
+        db.collection('posts').orderBy('fechaDeCreacion', 'desc').onSnapshot(
+            docs => {
+                let post = [];
+                docs.forEach( doc => {
+                    post.push({ 
+                        id: doc.id,
+                        data: doc.data(), 
+                });
+                });
+                setPosts(post);
+                setLoading(false)
+                console.log(post);
+            }
+        );
+    }, []);
+
+    return(
+        <View style={ {flex: 1}}>
+        {loading ? <ActivityIndicator size="large" color="#6C63FF"/> :
+            <View style={ {flex: 1}}>
+                <Text>Home</Text>
+                <FlatList
+                    data={posts}
+                    keyExtractor={item => item.id.toString()}
+                    renderItem={({ item }) => ( <Post data={item.data} id={item.id} navigation={props.navigation} />
+                    )}
+                />
+            </View>}
+        </View>
+    )
 }
